e906725915c90508f665172d3ace449049a8e45b
NerovnuiMihail <nerovnuimihail@gmail.com>
2022-05-20 12:27:38 +0300

    update NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,8 +1,20 @@
+import { useLocation, Link } from 'react-router-dom';
+import notfound from './404.png';
+
 import './NotFoundPage.css';
 
 const NotFoundPage = () => {
+    const location = useLocation();
+
     return (
-        <div></div>
+        <div className="not-found">
+            <img src={notfound} alt="notfound" />
+
+            <h1>Страница <span>{location.pathname ? location.pathname : null}</span> не найдена</h1>
+            <Link to="/">
+                <h2>Перейти на главную</h2>
+            </Link>
+        </div>
     )
 }
 
