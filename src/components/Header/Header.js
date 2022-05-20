e77e61489825b4df638c00bc4b1ecd03afbe6dca
NerovnuiMihail <nerovnuimihail@gmail.com>
2022-05-20 13:04:27 +0300

    update Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,29 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import logo from './logo.png';
+
 import './Header.css';
 
 
 const Header = () => {
+    const pizzaItems = useSelector(state => state.basket.basket.pizza);
+    const combosItems = useSelector(state => state.basket.basket.combos);
+    const snacksItems = useSelector(state => state.basket.basket.snacks);
+    const drinksItems = useSelector(state => state.basket.basket.drinks);
+    const dessertItems = useSelector(state => state.basket.basket.dessert);
+
+    const basketLength = () => {
+        const pizzaLength = pizzaItems.length ? pizzaItems.reduce((a, b) => a + b.count, 0) : 0;
+        const combosLength = combosItems.length ? combosItems.reduce((a, b) => a + b.count, 0) : 0;
+        const snacksLength = snacksItems.length ? snacksItems.reduce((a, b) => a + b.count, 0) : 0;
+        const drinksLength = drinksItems.length ? drinksItems.reduce((a, b) => a + b.count, 0) : 0;
+        const dessertLength = dessertItems.length ? dessertItems.reduce((a, b) => a + b.count, 0) : 0;
+
+        return pizzaLength + combosLength + snacksLength + drinksLength + dessertLength;
+    };
+
+    const counter = basketLength() ? " | " + basketLength() : null;
+
     return (
         <div className="header">
             <header className="header-wrapper content-wrapper">
@@ -30,7 +50,7 @@ const Header = () => {
                         to="/bonusactions" 
                         className={({ isActive }) => isActive ? "header-menu__item nav-active" : "header-menu__item"}>Акции</NavLink> 
                 </nav>
-                <NavLink to="/basket" className="basket">Корзина</NavLink>
+                <NavLink to="/basket" className="basket">Корзина {counter}</NavLink>
             </header>
         </div>
     )
